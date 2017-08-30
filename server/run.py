b20c5feae3497084c45b4367abdcc6bc91d4db19
Braden Jageman <bjageman@gmail.com>
2017-08-30 14:57:29 -0700

    fixing config

--- server/run.py
+++ server/run.py
@@ -9,4 +9,5 @@ db.create_all()
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     print(port)
+    print(os.environ.get('DB_USER', 'failed'))
     socketio.run(app, debug=True, port=port, host='0.0.0.0')
