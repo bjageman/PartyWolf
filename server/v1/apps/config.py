e51c436b8a54ca00e7bea0368594e66c2bbfbbcf
Braden Jageman <bjageman@gmail.com>
2017-08-30 14:14:43 -0700

    fixing config

---
+++ server/v1/apps/config.py
@@ -0,0 +1,12 @@
+import os
+
+DB_USER = (os.environ.get('DB_USER','root'))
+DB_PASS = (os.environ.get('DB_PASS','password'))
+DB_HOST = (os.environ.get('DB_HOST','host'))
+DB_DATABASE = (os.environ.get('DB_DATABASE','database'))
+DB_TEST = (os.environ.get('DB_TEST','test'))
+DB_TYPE = (os.environ.get('DB_TYPE','mysql'))
+DB_PORT = (os.environ.get('DB_PORT','3306'))
+
+DATABASE = DB_TYPE + '://' + DB_USER + ':' + DB_PASS + '@' + DB_HOST + ":" + DB_PORT + '/' + DB_DATABASE
+DATABASE_TEST = DB_TYPE + 'mysql://' + DB_USER + ':' + DB_PASS + '@' + DB_HOST + ":" + DB_PORT + '/' + DB_TEST
